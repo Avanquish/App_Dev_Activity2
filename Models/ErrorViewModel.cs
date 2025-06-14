046890b8da50f1bbf4b43e9a6c5f6b73ec750ac9
Justin Paul Palomiano Morada <justinpaulmorada969@gmail.com>
2025-06-14 19:05:51 +0800

    MVC First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace App_Dev_Activity2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
