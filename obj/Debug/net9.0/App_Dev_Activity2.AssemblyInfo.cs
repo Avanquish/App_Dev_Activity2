c8ebac2cd3844eaf71a7faa531f6d2b22b9d9b7e
Justin Paul Palomiano Morada <justinpaulmorada969@gmail.com>
2025-06-15 22:42:33 +0800

    Debug Layout

--- obj/Debug/net9.0/App_Dev_Activity2.AssemblyInfo.cs
+++ obj/Debug/net9.0/App_Dev_Activity2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App_Dev_Activity2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65ece35e23925b41da58cb7c48321643d2d88fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ff56ed2f0789f9ee7fb80ac62bc87642373cef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("App_Dev_Activity2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App_Dev_Activity2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
